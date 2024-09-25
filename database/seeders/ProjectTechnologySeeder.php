c7318079c41a797e49d1cbbe0db608479b68e1dc
BeneArp <benedetta.arpino@gmail.com>
2024-09-25 19:54:14 +0200

    feat: add pivot table

---
+++ database/seeders/ProjectTechnologySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Project;
+use App\Models\Technology;
+
+class ProjectTechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for($i = 0; $i < 10; $i++){
+            $project = Project::inRandomOrder()->first();
+
+            $technology_id = Technology::inRandomOrder()->first()->id;
+        }
+    }
+}
